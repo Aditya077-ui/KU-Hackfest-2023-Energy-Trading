5ad23d8e2fe80db5c7adcc04734b93e67b2c73bf
Aditya khadka <adityakhadka907@gmail.com>
2023-09-29 17:39:28 +0545

    navbar and hero section

---
+++ urja/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+    
+  );
+}
+
+export default App;
